6ac86b2577b63eaec60ce9f6a41345d3f0f9768b
India Spott <62669343+bleubirb@users.noreply.github.com>
2025-10-23 23:36:59 -0700

    disparity needs to be debugged

--- arms/src/vision/scripts/vision.py
+++ arms/src/vision/scripts/vision.py
@@ -3,17 +3,20 @@
 
 import csv
 import os
+import threading
 import time
 
 import cv2
+import numpy as np
 import rclpy
 from cv_bridge import CvBridge
 from detector import ExactDetector, run_detector_tiled
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 from rclpy.subscription import Subscription
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from tracker import CentroidTracker
+from stereo_msgs.msg import DisparityImage
 
 CHECKPOINT = os.path.join(os.path.abspath(os.path.dirname(__file__)), "bestestboy.pth")
 NAME = "blueberry"
@@ -30,13 +33,15 @@ TILE_OVERLAP = 0.2
 TILE_NMS = 0.5
 
 # stream content
-NO_DEPTH = True
-MAX_FPS = 0
+NO_DEPTH = False
+MAX_FPS = 0 # 0=unlimited
 
 # output
 CSV_FN = "blueberries_tiled"
 DATA_PATH = "data_cam"
 TRACK_MAX_DIST = 60
+
+IMAGE_TS_DELTA_THRESH = 0.05  # seconds
 
 
 class Vision:
@@ -48,7 +53,13 @@ class Vision:
             Image, "/vision/processed_image", 1
         )
         self.img_sub: Subscription = node.create_subscription(
-            Image, "/stereo/left/image_rect_color", self.process_image, 1
+            Image, "/stereo/left/image_rect_color", self.store_color_img, 1
+        )
+
+        self.disparity_sub: Subscription = node.create_subscription(DisparityImage, "/stereo/disparity", self.store_disparity_img, 1)
+
+        self.info_sub: Subscription = node.create_subscription(
+            CameraInfo, "/stereo/left/camera_info", self.store_camera_info, 1
         )
 
         self.br: CvBridge = CvBridge()
@@ -94,22 +105,103 @@ class Vision:
 
         self.last_tick: float = 0.0
 
-    def process_image(self, img_msg: Image) -> None:
+        self.color_dict: dict[float, np.ndarray] = {}
+        self.disparity_data: tuple[float, np.ndarray, float, float, float, float, float] | None = None
+
+        self.process_frames_thread = threading.Thread(target=self.process_frames_threaded)
+        self.process_frames_thread.start()
+
+    def store_camera_info(self, info_msg: CameraInfo) -> None:
+        self.node.destroy_subscription(self.info_sub)
+        self.node.get_logger().info("Camera info received and subscription closed.")
+
+        self.principal_point_u = info_msg.k[2]
+        self.principal_point_v = info_msg.k[5]
+
+        self.node.get_logger().info(
+            f"Camera principal point: u={self.principal_point_u}, v={self.principal_point_v}"
+        )
+
+    def store_color_img(self, img_msg: Image) -> None:
         ts: float = (
             float(img_msg.header.stamp.sec) + float(img_msg.header.stamp.nanosec) * 1e-9
         )
-        now = time.time()
-        self.frame += 1
-
         # Convert ROS Image message to OpenCV image
         cv_image = self.br.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
-        height, width, _ = cv_image.shape
 
-        self.node.get_logger().info(
-            f"Processing frame {self.frame} at time {ts:.3f}s with resolution {width}x{height}"
+        self.color_dict[ts] = cv_image.copy()
+
+        # self.node.get_logger().info(f"Stored color image at timestamp {ts:.3f}s")
+
+    def store_disparity_img(self, disp_msg: DisparityImage) -> None:
+        ts: float = (
+            float(disp_msg.header.stamp.sec) + float(disp_msg.header.stamp.nanosec) * 1e-9
         )
 
-        # detection (tiled or single)
+        if min(self.color_dict.keys(), default=float('inf')) > ts + IMAGE_TS_DELTA_THRESH:
+            self.node.get_logger().warning(
+                f"Disparity image at {ts:.3f}s is too old compared to color images; ignoring."
+            )
+            return
+
+        # Convert ROS Image message to OpenCV image
+        disp_image = self.br.imgmsg_to_cv2(disp_msg.image, desired_encoding="32FC1") # TODO: mono16?
+
+        self.disparity_data = (
+            ts,
+            disp_image.copy(),
+            disp_msg.delta_d,
+            disp_msg.valid_window.x_offset, # always 0
+            disp_msg.f,
+            disp_msg.t,
+            disp_msg.min_disparity,
+        )
+
+        # self.node.get_logger().info(f"Stored disparity image at timestamp {ts:.3f}s")
+
+        for ts_color in list(self.color_dict.keys()):
+            if ts_color < ts - IMAGE_TS_DELTA_THRESH:
+                # self.node.get_logger().warning(
+                #     f"Color image at {ts_color:.3f}s is too old compared to disparity images; removing."
+                # )
+                del self.color_dict[ts_color]
+
+
+    def process_frames(self) -> None:
+        if not self.disparity_data or not self.color_dict:
+            self.node.get_logger().info("Waiting for both color and disparity images...")
+            time.sleep(0.1)
+            return
+
+        # find current disparity timestamp
+        disp_ts, disp_image, scale, offset, focal_px, baseline_m, invalid_value = self.disparity_data
+
+        if disp_ts == self.last_tick:
+            # no new disparity image
+            time.sleep(0.01)
+            return
+        
+        # find closest color timestamp
+        color_ts = min(self.color_dict.keys(), key=lambda t: abs(t - disp_ts))
+
+        # check timestamp difference
+        if abs(color_ts - disp_ts) > 0.05:
+            self.node.get_logger().error(
+                f"Timestamp mismatch: color {color_ts:.3f}s vs disparity {disp_ts:.3f}s"
+            )
+            time.sleep(0.1)
+            return
+        # else:
+        #     self.node.get_logger().info(
+        #         f"Processing synchronized frames at {disp_ts:.3f}s"
+        #     )
+
+        # pop images and parameters from dicts
+        cv_image = self.color_dict.pop(color_ts)
+        
+        height, width, _ = cv_image.shape
+
+        # detection (tiled)
         dets_xywh, det_scores, _ = run_detector_tiled(
             self.detector,
             cv_image,
@@ -121,20 +213,16 @@ class Vision:
         )
         ids = self.tracker.update(dets_xywh) if dets_xywh else []
 
-        # TODO: depth estimation using stereo disparity (right now, this is difficult because disparity is a separate topic)
-        # has_disp = (not NO_DEPTH) and (imageset.disparity.image.data != b"")
-        # Zmap = None
-        # if has_disp:
-        #     disp = imageset.disparity
-        #     disp_img = to_numpy_image(disp.image)  # mono16
-        #     Zmap = disparity_to_depth_m(
-        #         disp_img,
-        #         disp.scale,
-        #         disp.offset,
-        #         left.focal_length,
-        #         disp.baseline,
-        #         disp.invalid_data_value,
-        #     )
+        Zmap = None
+        if not NO_DEPTH:
+            Zmap = self.disparity_to_depth_m(
+                disp_image,
+                scale,
+                offset,
+                focal_px,
+                baseline_m,
+                invalid_value,
+            )
 
         for i, xywh in enumerate(dets_xywh):
             x, y, w, h = map(int, xywh)
@@ -145,30 +233,30 @@ class Vision:
             # TODO: depth estimation
             X = Y = Zm = None
             depth_method = ""
-            # if Zmap is not None:
-            #     cx, cy = int(round(u)), int(round(v))
-            #     wx = max(1, int(round(w * 0.2)))
-            #     wy = max(1, int(round(h * 0.2)))
-            #     x0 = max(0, cx - wx)
-            #     x1 = min(width, cx + wx)
-            #     y0 = max(0, cy - wy)
-            #     y1 = min(height, cy + wy)
-            #     patch = Zmap[y0:y1, x0:x1]
-            #     if patch.size > 0:
-            #         Zm = float(np.nanmedian(patch))
-            #         if np.isfinite(Zm) and Zm > 0:
-            #             fx = float(left.focal_length)
-            #             cx0 = float(left.principal_point_u)
-            #             cy0 = float(left.principal_point_v)
-            #             X = (u - cx0) * Zm / fx
-            #             Y = (v - cy0) * Zm / fx
-            #             depth_method = "stereo"
+            if Zmap is not None:
+                cx, cy = int(round(u)), int(round(v))
+                wx = max(1, int(round(w * 0.2)))
+                wy = max(1, int(round(h * 0.2)))
+                x0 = max(0, cx - wx)
+                x1 = min(width, cx + wx)
+                y0 = max(0, cy - wy)
+                y1 = min(height, cy + wy)
+                patch = Zmap[y0:y1, x0:x1]
+                if patch.size > 0:
+                    Zm = float(np.nanmedian(patch))
+                    if np.isfinite(Zm) and Zm > 0:
+                        fx = float(focal_px)
+                        cx0 = float(self.principal_point_u)
+                        cy0 = float(self.principal_point_v)
+                        X = (u - cx0) * Zm / fx
+                        Y = (v - cy0) * Zm / fx
+                        depth_method = "stereo"
 
             self.annotate_image(cv_image, x, y, w, h, tid, NAME, score, Zm)
             self.csv_writer.writerow(
                 [
                     self.frame,
-                    f"{ts:.6f}",
+                    f"{disp_ts:.6f}",
                     tid,
                     NAME,
                     f"{score:.4f}",
@@ -185,36 +273,41 @@ class Vision:
                 ]
             )
 
-        fps = 1.0 / max(1e-6, (ts - self.last_tick))
-        self.last_tick = ts
+        fps = 1.0 / max(1e-9, (disp_ts - self.last_tick))
+        self.last_tick = disp_ts
 
-        if self.last_tick > 0.0:
-            self.node.get_logger().info(
-                f"Frame {self.frame}: Detected {len(dets_xywh)} objects, FPS: {fps:.2f}"
-            )
+        self.node.get_logger().info(
+            f"Frame {self.frame}: Detected {len(dets_xywh)} objects, FPS: {fps:.2f}"
+        )
+        self.frame += 1
 
         # Stream processed image back to publisher
         processed_img_msg: Image = self.br.cv2_to_imgmsg(cv_image, encoding="bgr8")
-        processed_img_msg.header.stamp = img_msg.header.stamp
+        processed_img_msg.header.stamp.sec = int(disp_ts)
+        processed_img_msg.header.stamp.nanosec = int((disp_ts - int(disp_ts)) * 1e9)
         self.processed_pub.publish(processed_img_msg)
 
-        if MAX_FPS > 0:
-            elapsed = time.time() - now
-            sleep_time = max(0.0, (1.0 / MAX_FPS) - elapsed)
-            time.sleep(sleep_time)
+    def process_frames_threaded(self) -> None:
+        while rclpy.ok():
+            start_time = time.time()
+            self.process_frames()
+            elapsed = time.time() - start_time
+            if MAX_FPS > 0:
+                sleep_time = max(0.0, (1.0 / MAX_FPS) - elapsed)
+                time.sleep(sleep_time)
 
-    # TODO: depth estimation
-    # def disparity_to_depth_m(
-    #     disp_u16: np.ndarray,
-    #     scale: float,
-    #     offset: float,
-    #     focal_px: float,
-    #     baseline_m: float,
-    #     invalid_value: float,
-    # ) -> np.ndarray:
-    #     d = (disp_u16.astype(np.float32) / float(scale)) + float(offset)
-    #     d[d <= max(1e-9, float(invalid_value))] = np.nan
-    #     return (float(focal_px) * float(baseline_m)) / d
+    def disparity_to_depth_m(
+        self,
+        disp_u16: np.ndarray,
+        scale: float,
+        offset: float,
+        focal_px: float,
+        baseline_m: float,
+        invalid_value: float,
+    ) -> np.ndarray:
+        d = (disp_u16.astype(np.float32) / float(scale)) + float(offset)
+        d[d <= max(1e-9, float(invalid_value))] = np.nan
+        return (float(focal_px) * float(baseline_m)) / d
 
     def annotate_image(self, cv_image, x, y, w, h, tid, name, score, Zm=None) -> None:
         color = (0, 220, 0) if Zm is not None else (80, 80, 80)
@@ -232,6 +325,7 @@ class Vision:
             2,
         )
 
+    
     def cleanup(self) -> None:
         self.csv_file.close()
 
